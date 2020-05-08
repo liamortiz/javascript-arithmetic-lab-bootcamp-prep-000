0adf50c355873414833be4514a256dfa21c17329
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 02:16:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function add(a, b) {return a + b}
 function substract(a, b) {return b - a}
 function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
+function increment(n) {return n++}
+function decrement(n) {return n--}
 
 function makeInt(string) {
   return parseInt(string, 10)
