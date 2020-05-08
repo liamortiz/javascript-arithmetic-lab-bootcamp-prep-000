48a04945366c7f4766bf57893bc04a5dcc2dbde1
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 02:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function add(a, b) {return a + b}
-function substract(a, b) {return a - b}
+function substract(a, b) {return b - a}
 function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
 
