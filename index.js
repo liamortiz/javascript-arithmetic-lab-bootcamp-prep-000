edeae508481cdf8cbeae6c29eeee3411e8d94278
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 02:20:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a, b) {return a + b}
 function subtract(a, b) {return a - b}
 function multiply(a, b) {return a * b}
 function divide(a, b) {return a / b}
-function increment(n) {return n++}
+function increment(n) {return n += 1}
 function decrement(n) {return n - 1}
 
 function makeInt(string) {
